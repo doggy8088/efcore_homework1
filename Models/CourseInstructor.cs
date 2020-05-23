8098804511c0d936ad360ce716f38745c7778c36
Will <doggy.huang@gmail.com>
2020-05-23 10:36:11 +0800

    執行 dotnet ef dbcontext scaffold 命令
    dotnet ef dbcontext scaffold "Server=(localdb)\MSSQLLocalDB;Database=ContosoUniversity;Trusted_Connection=True;MultipleActiveResultSets=true"  Microsoft.EntityFrameworkCore.SqlServer -o Models

---
+++ Models/CourseInstructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework1.Models
+{
+    public partial class CourseInstructor
+    {
+        public int CourseId { get; set; }
+        public int InstructorId { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Person Instructor { get; set; }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework1.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Course = new HashSet<Course>();
+        }
+
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartDate { get; set; }
+        public int? InstructorId { get; set; }
+        public byte[] RowVersion { get; set; }
+
+        public virtual Person Instructor { get; set; }
+        public virtual ICollection<Course> Course { get; set; }
+    }
+}
