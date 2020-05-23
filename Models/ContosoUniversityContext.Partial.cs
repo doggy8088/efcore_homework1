00185306258363741c5cf070431d7a8fd4602144
Will <doggy.huang@gmail.com>
2020-05-23 13:04:58 +0800

    自訂 SaveChanges() 的範例 (partial class)

---
+++ Models/ContosoUniversityContext.Partial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace homework1.Models
+{
+    public partial class ContosoUniversityContext : DbContext
+    {
+        public override int SaveChanges()
+        {
+            var entities = this.ChangeTracker.Entries();
+
+            foreach (var entry in entities)
+            {
+                Console.WriteLine("Entity Name: {0}", entry.Entity.GetType().FullName);
+                Console.WriteLine("Status: {0}", entry.State);
+
+                if (entry.State == EntityState.Deleted)
+                {
+                    entry.State = EntityState.Modified;
+                    entry.CurrentValues.SetValues(new { IsDeleted = true });
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.CurrentValues.SetValues(new { ModifiedOn = DateTime.Now });
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
