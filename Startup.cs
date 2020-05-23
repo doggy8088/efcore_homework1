a6238875bf08ccbed4d87b30ae7331ab3957abd4
Will <doggy.huang@gmail.com>
2020-05-23 12:27:31 +0800

    示範 InMemory 與 Sqlite 的 DI 設定方法

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,12 @@ namespace homework1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddDbContext<ContosoUniversityContext>(options =>
+            //     options.UseInMemoryDatabase("name"));
+
+            // services.AddDbContext<ContosoUniversityContext>(options =>
+            //     options.UseSqlite("Data Source=Database.db")));
+
             services.AddDbContext<ContosoUniversityContext>(options =>
             {
                 options
