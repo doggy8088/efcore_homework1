233b136cc695c802499c972c808956a0b58057d1
Will <doggy.huang@gmail.com>
2020-05-24 13:55:40 +0800

    加入 MyMVC 專案

---
+++ MyMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyMVC.Models;
+using MyMVC.Utils;
+
+namespace MyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CoursesClient client;
+
+        public HomeController(ILogger<HomeController> logger, CoursesClient client)
+        {
+            _logger = logger;
+            this.client = client;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Course()
+        {
+            var course = await this.client.GetCourseAsync(1);
+
+            return View(course);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
